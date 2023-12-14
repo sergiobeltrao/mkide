324a59aad773800e34d0d344ee329b712edbdeec
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-12-14 19:40:27 -0300

    feat: basic options added to the menu bar

--- src/main/java/mkide/com/mkide/Application.java
+++ src/main/java/mkide/com/mkide/Application.java
@@ -7,16 +7,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1152, 648);
-        stage.setTitle("Home");
+
+        Controller controller = fxmlLoader.getController();
+        controller.setStage(stage);
+
+        stage.setTitle("mkide");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
